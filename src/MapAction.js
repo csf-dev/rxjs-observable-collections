35c387db6c8b8d2e73e0a33573b6acc1c7f831b5
Craig Fowler <craig@csf-dev.com>
2018-11-24 16:18:01 +0000

    Implement an observable map

---
+++ src/MapAction.js
@@ -0,0 +1,76 @@
+//@flow
+import { ObservableMap } from './ObservableMap';
+
+export const MapActions = {
+    // Standard mutator functions
+    set: 'set',
+    clear: 'clear',
+    delete: 'delete',
+    // Extra mutation methods
+    replaceContents: 'replaceContents',
+    initialState: 'initialState'
+};
+
+export type MapActionName = $Keys<typeof MapActions>;
+
+export class MapAction<K : mixed, V : mixed> {
+    #type : MapActionName;
+    #map : ObservableMap<K,V>;
+
+    get type() { return this.#type; }
+    get map() : ObservableMap<K,V> { return this.#map; }
+
+    constructor(type : MapActionName, map : ObservableMap<K,V>) {
+        this.#type = type;
+        this.#map = map;
+    }
+}
+
+export class MapSetAction<K : mixed, V : mixed> extends MapAction<K,V> {
+    key : K
+    value : V;
+
+    constructor(map : ObservableMap<K,V>, key : K, value : V) {
+        super(MapActions.set, map);
+
+        this.key = key;
+        this.value = value;
+    }
+}
+
+export class MapClearAction<K : mixed, V : mixed> extends MapAction<K,V> {
+    previousValues : Map<K,V>;
+
+    constructor(map : ObservableMap<K,V>, previousValues : Map<K,V>) {
+        super(MapActions.clear, map);
+
+        this.previousValues = previousValues;
+    }
+}
+
+export class MapDeleteAction<K : mixed, V : mixed> extends MapAction<K,V> {
+    key : K
+
+    constructor(map : ObservableMap<K,V>, key : K) {
+        super(MapActions.delete, map);
+
+        this.key = key;
+    }
+}
+
+export class MapReplaceContentsAction<K : mixed, V : mixed> extends MapAction<K,V> {
+    previousValues : Map<K,V>;
+
+    constructor(map : ObservableMap<K,V>, previousValues : Map<K,V>) {
+        super(MapActions.replaceContents, map);
+
+        this.previousValues = previousValues;
+    }
+}
+
+export class MapInitialStateAction<K : mixed, V : mixed> extends MapAction<K,V> {
+    constructor(map : ObservableMap<K,V>) {
+        super(MapActions.initialState, map);
+    }
+}
+
---
+++ src/ObservableMap.js
@@ -0,0 +1,62 @@
+//@flow
+import * as a from './MapAction';
+import { BehaviorSubject } from 'rxjs';
+
+export class ObservableMap<K : mixed, V : mixed> extends Map<K,V> {
+    #subject : rxjs$Subject<a.MapAction<K,V>>;
+
+    get actions() : rxjs$Observable<a.MapAction<K,V>> { return this.#subject; }
+
+    set(key : K, value : V) : Map<K,V> {
+        super.set(key, value);
+        const action = new a.MapSetAction(this, key, value);
+        this.#subject.next(action);
+        return this;
+    }
+
+    clear() : void {
+        const previousValues = new Map(super.entries());
+        super.clear();
+        const action = new a.MapClearAction(this, previousValues);
+        this.#subject.next(action);
+    }
+
+    delete(key : K) : bool {
+        const deleted = super.delete(key);
+
+        if(deleted) {
+            const action = new a.MapDeleteAction(this, key);
+            this.#subject.next(action);
+        }
+
+        return deleted;
+    }
+
+    replaceContents(newContents : Iterable<[K,V]>) : Iterable<[K,V]> {
+        const previousValues = new Map(super.entries());
+
+        super.clear();
+        for (const kvp of newContents) super.set(kvp[0], kvp[1]);
+
+        const action = new a.MapReplaceContentsAction(this, previousValues);
+        this.#subject.next(action);
+        return previousValues;
+    }
+
+    constructor(source? : Iterable<[K,V]>) {
+        /* We can't call the original constructor of the superclass Set because, internally
+         * it makes use of this class' .add function, which we have overridden.
+         * 
+         * Instead, if there are source items, we manually add them via the superclass'
+         * own .add function.
+         */
+        super();
+
+        if(source) {
+            for (const kvp of source) super.set(kvp[0], kvp[1]);
+        }
+
+        const initialAction = new a.MapInitialStateAction<K,V>(this);
+        this.#subject = new BehaviorSubject(initialAction);
+    }
+}
